a7ddf83a10dc4e70963103193fb24dc27532ae3b
rsluchevskiy <roman.sluchevsky@gmail.com>
2023-01-16 15:38:34 +0500

    update

--- src/main/java/com/alibaba/fastjson/serializer/SimplePropertyPreFilter.java
+++ src/main/java/com/alibaba/fastjson/serializer/SimplePropertyPreFilter.java
@@ -1,3 +1,22 @@
+/*
+*    ------ BEGIN LICENSE ATTRIBUTION ------
+*    
+*    Portions of this file have been appropriated or derived from the following project(s) and therefore require attribution to the original licenses and authors.
+*    
+*    Release: https://github.com/google/guava/releases/tag/v10.0
+*    Source File: CustomConcurrentHashMap.java
+*    
+*    Copyrights:
+*      copyright (c) 2009 the guava authors
+*    
+*    Licenses:
+*      Apache License 2.0
+*      SPDXId: Apache-2.0
+*    
+*    Auto-attribution by Threatrix, Inc.
+*    
+*    ------ END LICENSE ATTRIBUTION ------
+*/
 package com.alibaba.fastjson.serializer;
 
 import java.util.HashSet;
