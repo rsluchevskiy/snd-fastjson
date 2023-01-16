8d1be8b7f80f8286424b0c25d3abd6361342ea2c
rsluchevskiy <roman.sluchevsky@gmail.com>
2023-01-16 15:39:00 +0500

    update

--- src/main/java/com/alibaba/fastjson/serializer/SerializerFeature.java
+++ src/main/java/com/alibaba/fastjson/serializer/SerializerFeature.java
@@ -1,4 +1,144 @@
 /*
+*    ------ BEGIN LICENSE ATTRIBUTION ------
+*    
+*    Portions of this file have been appropriated or derived from the following project(s) and therefore require attribution to the original licenses and authors.
+*    
+*    Release: https://github.com/google/guava/releases/tag/v6.0
+*    Source File: VisibleForTesting.java
+*    
+*    Copyrights:
+*      copyright (c) 2006 google inc
+*    
+*    Licenses:
+*      Apache License 2.0
+*      SPDXId: Apache-2.0
+*    
+*    
+*    Release: https://github.com/google/guava/releases/tag/v6.0
+*    Source File: Longs.java
+*    
+*    Copyrights:
+*      copyright (c) 2008 google inc
+*    
+*    Licenses:
+*      Apache License 2.0
+*      SPDXId: Apache-2.0
+*    
+*    
+*    Release: https://github.com/google/guava/releases/tag/v6.0
+*    Source File: Shorts.java
+*    
+*    Copyrights:
+*      copyright (c) 2008 google inc
+*    
+*    Licenses:
+*      Apache License 2.0
+*      SPDXId: Apache-2.0
+*    
+*    
+*    Release: https://github.com/google/guava/releases/tag/v6.0
+*    Source File: Ints.java
+*    
+*    Copyrights:
+*      copyright (c) 2008 google inc
+*    
+*    Licenses:
+*      Apache License 2.0
+*      SPDXId: Apache-2.0
+*    
+*    
+*    Release: https://github.com/google/guava/releases/tag/v6.0
+*    Source File: Chars.java
+*    
+*    Copyrights:
+*      copyright (c) 2008 google inc
+*    
+*    Licenses:
+*      Apache License 2.0
+*      SPDXId: Apache-2.0
+*    
+*    
+*    Release: https://github.com/google/guava/releases/tag/v6.0
+*    Source File: UnsignedBytes.java
+*    
+*    Copyrights:
+*      copyright (c) 2009 google inc
+*    
+*    Licenses:
+*      Apache License 2.0
+*      SPDXId: Apache-2.0
+*    
+*    
+*    Release: https://github.com/google/guava/releases/tag/v6.0
+*    Source File: SignedBytes.java
+*    
+*    Copyrights:
+*      copyright (c) 2009 google inc
+*    
+*    Licenses:
+*      Apache License 2.0
+*      SPDXId: Apache-2.0
+*    
+*    
+*    Release: https://github.com/google/guava/releases/tag/v6.0
+*    Source File: Bytes.java
+*    
+*    Copyrights:
+*      copyright (c) 2008 google inc
+*    
+*    Licenses:
+*      Apache License 2.0
+*      SPDXId: Apache-2.0
+*    
+*    
+*    Release: https://github.com/google/guava/releases/tag/v6.0
+*    Source File: Floats.java
+*    
+*    Copyrights:
+*      copyright (c) 2008 google inc
+*    
+*    Licenses:
+*      Apache License 2.0
+*      SPDXId: Apache-2.0
+*    
+*    
+*    Release: https://github.com/google/guava/releases/tag/v6.0
+*    Source File: Doubles.java
+*    
+*    Copyrights:
+*      copyright (c) 2008 google inc
+*    
+*    Licenses:
+*      Apache License 2.0
+*      SPDXId: Apache-2.0
+*    
+*    
+*    Release: https://github.com/google/guava/releases/tag/v6.0
+*    Source File: Booleans.java
+*    
+*    Copyrights:
+*      copyright (c) 2008 google inc
+*    
+*    Licenses:
+*      Apache License 2.0
+*      SPDXId: Apache-2.0
+*    
+*    
+*    Release: https://github.com/google/guava/releases/tag/v6.0
+*    Source File: Primitives.java
+*    
+*    Copyrights:
+*      copyright (c) 2007 google inc
+*    
+*    Licenses:
+*      Apache License 2.0
+*      SPDXId: Apache-2.0
+*    
+*    Auto-attribution by Threatrix, Inc.
+*    
+*    ------ END LICENSE ATTRIBUTION ------
+*/
+/*
  * Copyright 1999-2018 Alibaba Group.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
