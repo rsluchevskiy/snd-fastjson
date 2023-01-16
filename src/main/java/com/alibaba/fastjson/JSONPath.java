ad64c0685a05b232b7aab8cbace1326396375036
rsluchevskiy <roman.sluchevsky@gmail.com>
2023-01-16 15:38:59 +0500

    update

--- src/main/java/com/alibaba/fastjson/JSONPath.java
+++ src/main/java/com/alibaba/fastjson/JSONPath.java
@@ -1,3 +1,22 @@
+/*
+*    ------ BEGIN LICENSE ATTRIBUTION ------
+*    
+*    Portions of this file have been appropriated or derived from the following project(s) and therefore require attribution to the original licenses and authors.
+*    
+*    Release: https://github.com/google/guava/releases/tag/v31.1
+*    Source File: Maps.java
+*    
+*    Copyrights:
+*      copyright (c) 2007 the guava authors
+*    
+*    Licenses:
+*      Apache License 2.0
+*      SPDXId: Apache-2.0
+*    
+*    Auto-attribution by Threatrix, Inc.
+*    
+*    ------ END LICENSE ATTRIBUTION ------
+*/
 package com.alibaba.fastjson;
 
 import com.alibaba.fastjson.parser.DefaultJSONParser;
