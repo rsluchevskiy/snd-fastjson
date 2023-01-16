247b3e052b0c03204b072c747cfe32edf136ac94
rsluchevskiy <roman.sluchevsky@gmail.com>
2023-01-16 15:38:50 +0500

    update

--- src/main/java/com/alibaba/fastjson/JSONAware.java
+++ src/main/java/com/alibaba/fastjson/JSONAware.java
@@ -1,4 +1,298 @@
 /*
+*    ------ BEGIN LICENSE ATTRIBUTION ------
+*    
+*    Portions of this file have been appropriated or derived from the following project(s) and therefore require attribution to the original licenses and authors.
+*    
+*    Release: https://github.com/google/guava/releases/tag/v2.0
+*    Source File: Flushables.java
+*    
+*    Copyrights:
+*      copyright (c) 2007 google inc
+*    
+*    Licenses:
+*      Apache License 2.0
+*      SPDXId: Apache-2.0
+*    
+*    
+*    Release: https://github.com/google/guava/releases/tag/v6.0
+*    Source File: MultiInputStream.java
+*    
+*    Copyrights:
+*      copyright (c) 2007 google inc
+*    
+*    Licenses:
+*      Apache License 2.0
+*      SPDXId: Apache-2.0
+*    
+*    
+*    Release: https://github.com/google/guava/releases/tag/v6.0
+*    Source File: ImmutableAsList.java
+*    
+*    Copyrights:
+*      copyright (c) 2009 google inc
+*    
+*    Licenses:
+*      Apache License 2.0
+*      SPDXId: Apache-2.0
+*    
+*    
+*    Release: https://github.com/google/guava/releases/tag/v6.0
+*    Source File: ImmutableSortedSet.java
+*    
+*    Copyrights:
+*      copyright (c) 2008 google inc
+*    
+*    Licenses:
+*      Apache License 2.0
+*      SPDXId: Apache-2.0
+*    
+*    
+*    Release: https://github.com/google/guava/releases/tag/v6.0
+*    Source File: EnumHashBiMap.java
+*    
+*    Copyrights:
+*      copyright (c) 2007 google inc
+*    
+*    Licenses:
+*      Apache License 2.0
+*      SPDXId: Apache-2.0
+*    
+*    
+*    Release: https://github.com/google/guava/releases/tag/v2.0
+*    Source File: ByteProcessor.java
+*    
+*    Copyrights:
+*      copyright (c) 2009 google inc
+*    
+*    Licenses:
+*      Apache License 2.0
+*      SPDXId: Apache-2.0
+*    
+*    
+*    Release: https://github.com/google/guava/releases/tag/v2.0
+*    Source File: Closeables.java
+*    
+*    Copyrights:
+*      copyright (c) 2007 google inc
+*    
+*    Licenses:
+*      Apache License 2.0
+*      SPDXId: Apache-2.0
+*    
+*    
+*    Release: https://github.com/google/guava/releases/tag/v6.0
+*    Source File: ExpirationTimer.java
+*    
+*    Copyrights:
+*      copyright (c) 2009 google inc
+*    
+*    Licenses:
+*      Apache License 2.0
+*      SPDXId: Apache-2.0
+*    
+*    
+*    Release: https://github.com/google/guava/releases/tag/v6.0
+*    Source File: ImmutableSortedMap.java
+*    
+*    Copyrights:
+*      copyright (c) 2009 google inc
+*    
+*    Licenses:
+*      Apache License 2.0
+*      SPDXId: Apache-2.0
+*    
+*    
+*    Release: https://github.com/google/guava/releases/tag/v2.0
+*    Source File: MutableClassToInstanceMap.java
+*    
+*    Copyrights:
+*      copyright (c) 2007 google inc
+*    
+*    Licenses:
+*      Apache License 2.0
+*      SPDXId: Apache-2.0
+*    
+*    
+*    Release: https://github.com/google/guava/releases/tag/v2.0
+*    Source File: ImmutableClassToInstanceMap.java
+*    
+*    Copyrights:
+*      copyright (c) 2009 google inc
+*    
+*    Licenses:
+*      Apache License 2.0
+*      SPDXId: Apache-2.0
+*    
+*    
+*    Release: https://github.com/google/guava/releases/tag/v2.0
+*    Source File: LineProcessor.java
+*    
+*    Copyrights:
+*      copyright (c) 2009 google inc
+*    
+*    Licenses:
+*      Apache License 2.0
+*      SPDXId: Apache-2.0
+*    
+*    
+*    Release: https://github.com/google/guava/releases/tag/v6.0
+*    Source File: LineBuffer.java
+*    
+*    Copyrights:
+*      copyright (c) 2007 google inc
+*    
+*    Licenses:
+*      Apache License 2.0
+*      SPDXId: Apache-2.0
+*    
+*    
+*    Release: https://github.com/google/guava/releases/tag/v6.0
+*    Source File: ByteArrayDataInput.java
+*    
+*    Copyrights:
+*      copyright (c) 2009 google inc
+*    
+*    Licenses:
+*      Apache License 2.0
+*      SPDXId: Apache-2.0
+*    
+*    
+*    Release: https://github.com/google/guava/releases/tag/v6.0
+*    Source File: Finalizer.java
+*    
+*    Copyrights:
+*      copyright (c) 2008 google inc
+*    
+*    Licenses:
+*      Apache License 2.0
+*      SPDXId: Apache-2.0
+*    
+*    
+*    Release: https://github.com/google/guava/releases/tag/v6.0
+*    Source File: InputSupplier.java
+*    
+*    Copyrights:
+*      copyright (c) 2007 google inc
+*    
+*    Licenses:
+*      Apache License 2.0
+*      SPDXId: Apache-2.0
+*    
+*    
+*    Release: https://github.com/google/guava/releases/tag/v2.0
+*    Source File: NullOutputStream.java
+*    
+*    Copyrights:
+*      copyright (c) 2004 google inc
+*    
+*    Licenses:
+*      Apache License 2.0
+*      SPDXId: Apache-2.0
+*    
+*    
+*    Release: https://github.com/google/guava/releases/tag/v2.0
+*    Source File: CountingOutputStream.java
+*    
+*    Copyrights:
+*      copyright (c) 2007 google inc
+*    
+*    Licenses:
+*      Apache License 2.0
+*      SPDXId: Apache-2.0
+*    
+*    
+*    Release: https://github.com/google/guava/releases/tag/v6.0
+*    Source File: NullOutputException.java
+*    
+*    Copyrights:
+*      copyright (c) 2009 google inc
+*    
+*    Licenses:
+*      Apache License 2.0
+*      SPDXId: Apache-2.0
+*    
+*    
+*    Release: https://github.com/google/guava/releases/tag/v6.0
+*    Source File: AppendableWriter.java
+*    
+*    Copyrights:
+*      copyright (c) 2006 google inc
+*    
+*    Licenses:
+*      Apache License 2.0
+*      SPDXId: Apache-2.0
+*    
+*    
+*    Release: https://github.com/google/guava/releases/tag/v2.0
+*    Source File: package-info.java
+*    
+*    Copyrights:
+*      copyright (c) 2007 google inc
+*    
+*    Licenses:
+*      Apache License 2.0
+*      SPDXId: Apache-2.0
+*    
+*    
+*    Release: https://github.com/google/guava/releases/tag/v6.0
+*    Source File: ByteArrayDataOutput.java
+*    
+*    Copyrights:
+*      copyright (c) 2009 google inc
+*    
+*    Licenses:
+*      Apache License 2.0
+*      SPDXId: Apache-2.0
+*    
+*    
+*    Release: https://github.com/google/guava/releases/tag/v2.0
+*    Source File: CountingInputStream.java
+*    
+*    Copyrights:
+*      copyright (c) 2007 google inc
+*    
+*    Licenses:
+*      Apache License 2.0
+*      SPDXId: Apache-2.0
+*    
+*    
+*    Release: https://github.com/google/guava/releases/tag/v6.0
+*    Source File: EnumBiMap.java
+*    
+*    Copyrights:
+*      copyright (c) 2007 google inc
+*    
+*    Licenses:
+*      Apache License 2.0
+*      SPDXId: Apache-2.0
+*    
+*    
+*    Release: https://github.com/google/guava/releases/tag/v6.0
+*    Source File: AsynchronousComputationException.java
+*    
+*    Copyrights:
+*      copyright (c) 2009 google inc
+*    
+*    Licenses:
+*      Apache License 2.0
+*      SPDXId: Apache-2.0
+*    
+*    
+*    Release: https://github.com/google/guava/releases/tag/v6.0
+*    Source File: OutputSupplier.java
+*    
+*    Copyrights:
+*      copyright (c) 2007 google inc
+*    
+*    Licenses:
+*      Apache License 2.0
+*      SPDXId: Apache-2.0
+*    
+*    Auto-attribution by Threatrix, Inc.
+*    
+*    ------ END LICENSE ATTRIBUTION ------
+*/
+/*
  * Copyright 1999-2017 Alibaba Group.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
