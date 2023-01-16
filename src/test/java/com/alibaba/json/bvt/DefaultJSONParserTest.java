522b03546a36750711e62ccad441aeaa85b022d8
rsluchevskiy <roman.sluchevsky@gmail.com>
2023-01-16 15:38:55 +0500

    update

--- src/test/java/com/alibaba/json/bvt/DefaultJSONParserTest.java
+++ src/test/java/com/alibaba/json/bvt/DefaultJSONParserTest.java
@@ -1,4 +1,24 @@
 /*
+*    ------ BEGIN LICENSE ATTRIBUTION ------
+*    
+*    Portions of this file have been appropriated or derived from the following project(s) and therefore require attribution to the original licenses and authors.
+*    
+*    Project: https://spring.io/projects/spring-boot
+*    Release: https://github.com/spring-projects/spring-boot/releases/tag/v2.1.1.RELEASE
+*    Source File: ExitCodeGenerators.java
+*    
+*    Copyrights:
+*      copyright 2012-2018 the original author or authors
+*    
+*    Licenses:
+*      Apache License 2.0
+*      SPDXId: Apache-2.0
+*    
+*    Auto-attribution by Threatrix, Inc.
+*    
+*    ------ END LICENSE ATTRIBUTION ------
+*/
+/*
  * Copyright 1999-2017 Alibaba Group.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
