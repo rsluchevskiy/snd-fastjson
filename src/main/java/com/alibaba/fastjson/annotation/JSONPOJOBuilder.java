8ed4890e578fe88f980ff4b0f7f046181f0f02e1
rsluchevskiy <roman.sluchevsky@gmail.com>
2023-01-16 15:38:11 +0500

    update

--- src/main/java/com/alibaba/fastjson/annotation/JSONPOJOBuilder.java
+++ src/main/java/com/alibaba/fastjson/annotation/JSONPOJOBuilder.java
@@ -1,3 +1,22 @@
+/*
+*    ------ BEGIN LICENSE ATTRIBUTION ------
+*    
+*    Portions of this file have been appropriated or derived from the following project(s) and therefore require attribution to the original licenses and authors.
+*    
+*    Release: https://github.com/FasterXML/jackson-databind/releases/tag/jackson-databind-2.14.1
+*    Source File: JsonPOJOBuilder.java
+*    
+*    Copyrights:
+*      copyright (c) 2007- tatu saloranta, tatu.saloranta@iki.fi
+*    
+*    Licenses:
+*      Apache License 2.0
+*      SPDXId: Apache-2.0
+*    
+*    Auto-attribution by Threatrix, Inc.
+*    
+*    ------ END LICENSE ATTRIBUTION ------
+*/
 package com.alibaba.fastjson.annotation;
 
 import java.lang.annotation.ElementType;
