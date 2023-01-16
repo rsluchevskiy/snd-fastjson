a769d6076b617d68dd0eea44c2685e119ec4e967
rsluchevskiy <roman.sluchevsky@gmail.com>
2023-01-16 15:38:28 +0500

    update

--- src/test/java/com/alibaba/json/test/JSONParser2Test.java
+++ src/test/java/com/alibaba/json/test/JSONParser2Test.java
@@ -1,4 +1,24 @@
 /*
+*    ------ BEGIN LICENSE ATTRIBUTION ------
+*    
+*    Portions of this file have been appropriated or derived from the following project(s) and therefore require attribution to the original licenses and authors.
+*    
+*    Project: https://spring.io/projects/spring-boot
+*    Release: https://github.com/spring-projects/spring-boot/releases/tag/v1.0.0.RC4
+*    Source File: SampleSecureApplication.java
+*    
+*    Copyrights:
+*      copyright 2012-2014 the original author or authors
+*    
+*    Licenses:
+*      Apache License 2.0
+*      SPDXId: Apache-2.0
+*    
+*    Auto-attribution by Threatrix, Inc.
+*    
+*    ------ END LICENSE ATTRIBUTION ------
+*/
+/*
  * Copyright 1999-2017 Alibaba Group.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
