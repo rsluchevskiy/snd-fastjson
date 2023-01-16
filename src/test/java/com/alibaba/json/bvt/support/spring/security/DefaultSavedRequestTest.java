0d700122f73b26b500a4127ed8fc0ee0136bec1f
rsluchevskiy <roman.sluchevsky@gmail.com>
2023-01-16 15:38:04 +0500

    update

--- src/test/java/com/alibaba/json/bvt/support/spring/security/DefaultSavedRequestTest.java
+++ src/test/java/com/alibaba/json/bvt/support/spring/security/DefaultSavedRequestTest.java
@@ -1,3 +1,23 @@
+/*
+*    ------ BEGIN LICENSE ATTRIBUTION ------
+*    
+*    Portions of this file have been appropriated or derived from the following project(s) and therefore require attribution to the original licenses and authors.
+*    
+*    Project: https://spring.io/projects/spring-boot
+*    Release: https://github.com/spring-projects/spring-boot/releases/tag/v1.0.0.RC4
+*    Source File: SampleSecureApplication.java
+*    
+*    Copyrights:
+*      copyright 2012-2014 the original author or authors
+*    
+*    Licenses:
+*      Apache License 2.0
+*      SPDXId: Apache-2.0
+*    
+*    Auto-attribution by Threatrix, Inc.
+*    
+*    ------ END LICENSE ATTRIBUTION ------
+*/
 package com.alibaba.json.bvt.support.spring.security;
 
 import com.alibaba.fastjson.JSON;
