74593e1e8bab598eb8e00a92255cdbff2a594431
rsluchevskiy <roman.sluchevsky@gmail.com>
2023-01-16 15:39:03 +0500

    update

--- src/main/java/com/alibaba/fastjson/parser/Feature.java
+++ src/main/java/com/alibaba/fastjson/parser/Feature.java
@@ -1,4 +1,243 @@
 /*
+*    ------ BEGIN LICENSE ATTRIBUTION ------
+*    
+*    Portions of this file have been appropriated or derived from the following project(s) and therefore require attribution to the original licenses and authors.
+*    
+*    Release: https://github.com/google/guava/releases/tag/v6.0
+*    Source File: Objects.java
+*    
+*    Copyrights:
+*      copyright (c) 2007 google inc
+*    
+*    Licenses:
+*      Apache License 2.0
+*      SPDXId: Apache-2.0
+*    
+*    
+*    Release: https://github.com/google/guava/releases/tag/v2.0
+*    Source File: FinalizableSoftReference.java
+*    
+*    Copyrights:
+*      copyright (c) 2007 google inc
+*    
+*    Licenses:
+*      Apache License 2.0
+*      SPDXId: Apache-2.0
+*    
+*    
+*    Release: https://github.com/google/guava/releases/tag/v2.0
+*    Source File: Defaults.java
+*    
+*    Copyrights:
+*      copyright (c) 2007 google inc
+*    
+*    Licenses:
+*      Apache License 2.0
+*      SPDXId: Apache-2.0
+*    
+*    
+*    Release: https://github.com/google/guava/releases/tag/v2.0
+*    Source File: FinalizablePhantomReference.java
+*    
+*    Copyrights:
+*      copyright (c) 2007 google inc
+*    
+*    Licenses:
+*      Apache License 2.0
+*      SPDXId: Apache-2.0
+*    
+*    
+*    Release: https://github.com/google/guava/releases/tag/v6.0
+*    Source File: Platform.java
+*    
+*    Copyrights:
+*      copyright (c) 2009 google inc
+*    
+*    Licenses:
+*      Apache License 2.0
+*      SPDXId: Apache-2.0
+*    
+*    
+*    Release: https://github.com/google/guava/releases/tag/v2.0
+*    Source File: FinalizableReferenceQueue.java
+*    
+*    Copyrights:
+*      copyright (c) 2007 google inc
+*    
+*    Licenses:
+*      Apache License 2.0
+*      SPDXId: Apache-2.0
+*    
+*    
+*    Release: https://github.com/google/guava/releases/tag/v6.0
+*    Source File: Splitter.java
+*    
+*    Copyrights:
+*      copyright (c) 2009 google inc
+*    
+*    Licenses:
+*      Apache License 2.0
+*      SPDXId: Apache-2.0
+*    
+*    
+*    Release: https://github.com/google/guava/releases/tag/v2.0
+*    Source File: CharMatcher.java
+*    
+*    Copyrights:
+*      copyright (c) 2008 google inc
+*    
+*    Licenses:
+*      Apache License 2.0
+*      SPDXId: Apache-2.0
+*    
+*    
+*    Release: https://github.com/google/guava/releases/tag/v2.0
+*    Source File: Function.java
+*    
+*    Copyrights:
+*      copyright (c) 2007 google inc
+*    
+*    Licenses:
+*      Apache License 2.0
+*      SPDXId: Apache-2.0
+*    
+*    
+*    Release: https://github.com/google/guava/releases/tag/v2.0
+*    Source File: Charsets.java
+*    
+*    Copyrights:
+*      copyright (c) 2007 google inc
+*    
+*    Licenses:
+*      Apache License 2.0
+*      SPDXId: Apache-2.0
+*    
+*    
+*    Release: https://github.com/google/guava/releases/tag/v6.0
+*    Source File: Throwables.java
+*    
+*    Copyrights:
+*      copyright (c) 2007 google inc
+*    
+*    Licenses:
+*      Apache License 2.0
+*      SPDXId: Apache-2.0
+*    
+*    
+*    Release: https://github.com/google/guava/releases/tag/v2.0
+*    Source File: Functions.java
+*    
+*    Copyrights:
+*      copyright (c) 2007 google inc
+*    
+*    Licenses:
+*      Apache License 2.0
+*      SPDXId: Apache-2.0
+*    
+*    
+*    Release: https://github.com/google/guava/releases/tag/v6.0
+*    Source File: Service.java
+*    
+*    Copyrights:
+*      copyright (c) 2009 google inc
+*    
+*    Licenses:
+*      Apache License 2.0
+*      SPDXId: Apache-2.0
+*    
+*    
+*    Release: https://github.com/google/guava/releases/tag/v6.0
+*    Source File: Supplier.java
+*    
+*    Copyrights:
+*      copyright (c) 2007 google inc
+*    
+*    Licenses:
+*      Apache License 2.0
+*      SPDXId: Apache-2.0
+*    
+*    
+*    Release: https://github.com/google/guava/releases/tag/v6.0
+*    Source File: Predicate.java
+*    
+*    Copyrights:
+*      copyright (c) 2007 google inc
+*    
+*    Licenses:
+*      Apache License 2.0
+*      SPDXId: Apache-2.0
+*    
+*    
+*    Release: https://github.com/google/guava/releases/tag/v6.0
+*    Source File: Preconditions.java
+*    
+*    Copyrights:
+*      copyright (c) 2007 google inc
+*    
+*    Licenses:
+*      Apache License 2.0
+*      SPDXId: Apache-2.0
+*    
+*    
+*    Release: https://github.com/google/guava/releases/tag/v2.0
+*    Source File: FinalizableWeakReference.java
+*    
+*    Copyrights:
+*      copyright (c) 2007 google inc
+*    
+*    Licenses:
+*      Apache License 2.0
+*      SPDXId: Apache-2.0
+*    
+*    
+*    Release: https://github.com/google/guava/releases/tag/v2.0
+*    Source File: FinalizableReference.java
+*    
+*    Copyrights:
+*      copyright (c) 2007 google inc
+*    
+*    Licenses:
+*      Apache License 2.0
+*      SPDXId: Apache-2.0
+*    
+*    
+*    Release: https://github.com/google/guava/releases/tag/v2.0
+*    Source File: CaseFormat.java
+*    
+*    Copyrights:
+*      copyright (c) 2006 google inc
+*    
+*    Licenses:
+*      Apache License 2.0
+*      SPDXId: Apache-2.0
+*    
+*    
+*    Release: https://github.com/google/guava/releases/tag/v2.0
+*    Source File: Joiner.java
+*    
+*    Copyrights:
+*      copyright (c) 2008 google inc
+*    
+*    Licenses:
+*      Apache License 2.0
+*      SPDXId: Apache-2.0
+*    
+*    
+*    Release: https://github.com/google/guava/releases/tag/v6.0
+*    Source File: Suppliers.java
+*    
+*    Copyrights:
+*      copyright (c) 2007 google inc
+*    
+*    Licenses:
+*      Apache License 2.0
+*      SPDXId: Apache-2.0
+*    
+*    Auto-attribution by Threatrix, Inc.
+*    
+*    ------ END LICENSE ATTRIBUTION ------
+*/
+/*
  * Copyright 1999-2017 Alibaba Group.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
