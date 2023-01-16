f128076dedfb13d42b8a0bb45b6e76bf1c37fe34
rsluchevskiy <roman.sluchevsky@gmail.com>
2023-01-16 15:38:47 +0500

    update

--- src/test/java/com/alibaba/json/test/DigitTest.java
+++ src/test/java/com/alibaba/json/test/DigitTest.java
@@ -1,4 +1,23 @@
 /*
+*    ------ BEGIN LICENSE ATTRIBUTION ------
+*    
+*    Portions of this file have been appropriated or derived from the following project(s) and therefore require attribution to the original licenses and authors.
+*    
+*    Release: https://github.com/google/guava/releases/tag/jdk5-backport-v17.0-post
+*    Source File: Invokable.java
+*    
+*    Copyrights:
+*      copyright (c) 2012 the guava authors
+*    
+*    Licenses:
+*      Apache License 2.0
+*      SPDXId: Apache-2.0
+*    
+*    Auto-attribution by Threatrix, Inc.
+*    
+*    ------ END LICENSE ATTRIBUTION ------
+*/
+/*
  * Copyright 1999-2017 Alibaba Group.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
